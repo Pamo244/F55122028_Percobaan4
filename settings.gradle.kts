233a1204c1f4c79a1decbe4aeee4a7ce2922e807
Pamo244 <andrikurniawan68310@gmail.com>
2023-10-18 14:44:31 +0800

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "F55122028"
+include(":app")
+ 
